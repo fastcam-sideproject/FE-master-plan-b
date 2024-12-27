d1b744fb34ba5ddb699169ed461afd195b8854f0
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-12-28 04:02:59 +0900

    chore : 초기 환경 세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Master Plan B",
+  description: "자격증 정보",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <main>안녕하세요 마스터 플랜 비 입니다</main>
+  );
+}
