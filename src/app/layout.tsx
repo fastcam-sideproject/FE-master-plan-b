63f410f5256ba61e50894e4cabf7979cabbc37f1
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-12-28 09:08:16 +0900

    feat: eslint 8버전으로 재설치 및 eslint 추가 설정 설치, prettier 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
-import type { Metadata } from "next";
-import "./globals.css";
-
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Master Plan B",
-  description: "자격증 정보",
+  title: 'Master Plan B',
+  description: '자격증 정보',
 };
 
 export default function RootLayout({
@@ -14,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <main>안녕하세요 마스터 플랜 비 입니다</main>
-  );
+  return <main>안녕하세요 마스터 플랜 비 입니다</main>;
 }
