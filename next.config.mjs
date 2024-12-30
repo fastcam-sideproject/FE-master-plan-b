3ae21782b10a7293f5b1cf97e2b03b992fa0ba05
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-12-30 18:19:20 +0900

    chore: next 14.2.5, react 18, msw 2.4.3 버전으로 다운그레이드 적용

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), '_http_common'];
+      config.target = 'node';
+    }
+
+    return config;
+  },
+};
+
+export default nextConfig;
