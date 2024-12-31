6e198cb75b23e58eb810e1ff4984782004b835f0
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-12-31 16:27:51 +0900

    feat : footer 목업 완성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import appleSdGothic from '@/components/AppleSdGothic';
 import MSWComponent from '@/components/MSWComponent';
 import ReactQueryProviders from '@/hooks/useReactQuery';
+import Footer from '@/components/Footer/Footer';
 
 export const metadata: Metadata = {
   title: 'Master Plan B',
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={`${appleSdGothic.className}`} suppressHydrationWarning={true}>
         <ReactQueryProviders>
           <MSWComponent>{children}</MSWComponent>
+          <Footer />
         </ReactQueryProviders>
       </body>
     </html>
