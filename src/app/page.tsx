4b7d40cfb99f12c2e4db53d86577d30314b2463f
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-12-31 18:19:42 +0900

    feat: button secondary 속성 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <h2 className="text-heading-medium-desktop md:text-heading-medium-mobile font-extrabold">
         중제목asdasdsaddddddddd
       </h2>
-      <main className="bg-yellow-10 border shadow-1 stroke-normal rounded-10 font-semibold">
+      <main className="bg-neutral-10 border shadow-1 stroke-normal rounded-10 font-semibold">
         안녕하세요 마스터 플랜 비 입니다
-        <Button variant="default" size="lg">
+        <Button variant="secondary" size="sm">
           클릭하세요.
         </Button>
         <button className="text-label-large-desktop border rounded-2 border-b-gray-50 ">aa</button>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,23 +4,24 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300',
+  'inline-flex items-center justify-center whitespace-nowrap ring-offset-white transition-colors  disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300',
   {
     variants: {
       variant: {
         default:
-          'text-neutral-85 font-[400] bg-primary-40 hover:bg-primary-50 active:bg-primary-60 text-label-large-desktop',
+          'text-neutral-85 font-[400] bg-primary-40 hover:bg-primary-50 active:bg-primary-60 focus:bg-neutral-5 text-label-large-desktop ',
+        secondary:
+          'text-neutral-0 !text-neutral-0 font-[400] bg-neutral-85 hover:bg-neutral-80 active:bg-neutral-70 text-label-large-desktop',
         outline:
           'border border-neutral-200 bg-white hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
-        secondary:
-          'bg-neutral-100 text-neutral-900 hover:bg-neutral-100/80 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80',
         ghost:
           'hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
         link: 'text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50',
       },
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
+        sm: 'rounded-3 px-[16px] py-[8px] gap-[10px]',
+        md: 'rounded-4 px-[24px] py-[12px] gap-[10px]',
         lg: 'rounded-4 px-[32px] py-[16px] gap-[10px]',
         icon: 'h-10 w-10',
       },
