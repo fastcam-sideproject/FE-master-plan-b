4b5ed22314924546a4e6bc6d90938ff440ddea25
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-12-31 12:59:20 +0900

    feat: default large button 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
       <h2 className="text-heading-medium-desktop md:text-heading-medium-mobile font-extrabold">
         중제목asdasdsaddddddddd
       </h2>
-      <p className="text-title-large- md:text-body-large-mobile font-medium leading-[160%]">
-        본문 텍스트
-      </p>
       <main className="bg-yellow-10 border shadow-1 stroke-normal rounded-10 font-semibold">
         안녕하세요 마스터 플랜 비 입니다
-        <Button>Click me</Button>
+        <Button variant="default" size="lg">
+          클릭하세요.
+        </Button>
+        <button className="text-label-large-desktop border rounded-2 border-b-gray-50 ">aa</button>
       </main>
     </>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,18 +1,15 @@
 import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
-
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300',
+  'inline-flex items-center justify-center whitespace-nowrap ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300',
   {
     variants: {
       variant: {
         default:
-          'bg-neutral-900 text-neutral-50 hover:bg-neutral-900/90 dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-neutral-50/90',
-        destructive:
-          'bg-red-500 text-neutral-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-neutral-50 dark:hover:bg-red-900/90',
+          'text-neutral-85 font-[400] bg-yellow-40 hover:bg-yellow-50 active:bg-yellow-60 text-label-large-desktop',
         outline:
           'border border-neutral-200 bg-white hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
         secondary:
@@ -24,7 +21,7 @@ const buttonVariants = cva(
       size: {
         default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
+        lg: 'rounded-4 px-[32px] py-[16px] gap-[10px]',
         icon: 'h-10 w-10',
       },
     },
