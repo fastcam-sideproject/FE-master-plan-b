7d3c921648e6c27fa81df9f70dcdc0325cb3c1ee
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2025-01-02 12:19:22 +0900

    feat: FAB컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Fab from '@/components/FAB/Fab';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <>
       <h1 className="text-display-desktop md:text-display-mobile font-extrabold">제목</h1>
+      <Fab />
 
       <main className="bg-neutral-10 border shadow-1 stroke-normal rounded-10 font-semibold">
         안녕하세요 마스터 플랜 비 입니다
---
+++ src/components/FAB/Fab.tsx
@@ -0,0 +1,15 @@
+import { IconPencil } from '@tabler/icons-react';
+
+function Fab() {
+  return (
+    <button
+      className="w-[64px] h-[64px] p-[20px] rounded-10 border stroke-normal border-neutral-10 bg-neutral-0 shadow-[0px_0px_15px_0px_rgba(70,69,107,0.10)]"
+      aria-label="edit button"
+      type="button"
+    >
+      <IconPencil width={24} height={24} />
+    </button>
+  );
+}
+
+export default Fab;
