18b0a72bbc69e307ddc87fc9b279015b9716ef85
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2025-01-16 20:39:27 +0900

    refactor : css 수정

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -11,14 +11,14 @@ export default function SideBar() {
   };
 
   return (
-    <aside className={`transition-all duration-300 ${isCollapsed ? "w-[80px]" : "w-[280px]"
+    <aside className={`transition-all duration-300 ${isCollapsed ? "w-[72px]" : "w-[280px]"
       } h-[100vh] bg-neutral-85 pb-6`}>
       <nav className="h-full flex flex-col justify-between">
         <div>
-          <div className={`${isCollapsed ? "relative" : ""} flex justify-between items-center p-7`}>
+          <div className={`${isCollapsed ? "relative p-5" : " p-7"} flex justify-between items-center`}>
             <Image width={`${isCollapsed ? "40" : "157"}`} height={40} src={`${isCollapsed ? "/logos/logo-light.svg" : "/logos/logo-text-light.svg"}`} alt="로고" />
-            <div className={`${isCollapsed ? "w-8 h-8 p-2 absolute top-16 -right-4 bg-neutral-80 rounded-full" : ""}`}>
-              <Image width={24} height={24} src={`${isCollapsed ? "/icons/direction/bar-right.svg" : "/icons/direction/bar-left.svg"}`} alt="화살표" className={`${isCollapsed ? "filter invert" : "filter invert"}`} onClick={toggleSidebar} />
+            <div className={`${isCollapsed ? "w-8 h-8 p-2 absolute top-16 -right-4 bg-neutral-80 rounded-full" : ""} hover:cursor-pointer`}>
+              <Image width={24} height={24} src={`${isCollapsed ? "/icons/direction/bar-right.svg" : "/icons/direction/bar-left.svg"}`} alt="화살표" className="filter invert" onClick={toggleSidebar} />
             </div>
           </div>
           <ul className="flex flex-col justify-center items-center text-neutral-0">
