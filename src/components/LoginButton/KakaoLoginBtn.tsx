9c28e4a68a826b594107f788c6b9171bd6ded40e
wook <yho7955@naver.com>
2025-01-31 19:41:02 +0900

    refactor: 버튼 태그으로 수정

--- src/components/LoginButton/KakaoLoginBtn.tsx
+++ src/components/LoginButton/KakaoLoginBtn.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 export function KakaoLoginBtn() {
   return (
-    <div className="w-40 mobile:w-[335px] h-[180px] mobile:h-[65px] px-6 py-9 mobile:p-4 bg-[#FFE000] rounded-4 flex-col justify-center items-center inline-flex">
+    <button
+      className="w-40 mobile:w-[335px] h-[180px] mobile:h-[65px] px-6 py-9 mobile:p-4 bg-[#FFE000] rounded-4 flex-col justify-center items-center inline-flex"
+      type="button"
+    >
       <div className="flex flex-col justify-start items-center gap-6 mobile:flex-row mobile:gap-4">
         <img src="/icons/login/kakao.svg" alt="kakao login icon" className="w-8 h-[28.69px]" />
         <div className="text-neutral-85 text-label-medium-desktop font-normal">
           카카오로 시작하기
         </div>
       </div>
-    </div>
+    </button>
   );
 }
