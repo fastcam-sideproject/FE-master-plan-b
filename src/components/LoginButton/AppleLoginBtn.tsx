0c113b1891da80b2f4fbfcfe5d1d681aabb4da56
wook <yho7955@naver.com>
2025-01-31 19:40:27 +0900

    feat: kakaoLoginButton 컴포넌트 추가

---
+++ src/components/LoginButton/AppleLoginBtn.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function AppleLoginBtn() {
+  return <div>AppleLoginBtn</div>;
+}
---
+++ src/components/LoginButton/GoogleLoginBtn.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function GoogleLoginBtn() {
+  return <div>GoogleLoginBtn</div>;
+}
