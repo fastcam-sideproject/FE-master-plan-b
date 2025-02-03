f87724dae9a79e65ae28aaf39d03e0bf3ff0f8ad
wook <yho7955@naver.com>
2025-02-03 15:34:00 +0900

    style: kakao login button컴포넌트 padding 추가

--- src/components/LoginButton/KakaoLoginBtn.tsx
+++ src/components/LoginButton/KakaoLoginBtn.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 export function KakaoLoginBtn() {
   return (
     <button
-      className="w-40 mobile:w-[335px] h-[180px] mobile:h-[65px] px-6 py-9 mobile:p-4 bg-[#FFE000] rounded-4 flex-col justify-center items-center inline-flex"
+      className="w-40 mobile:w-[335px] h-[180px] mobile:h-[65px] px-6 py-9 mobile:px-4 mobile:py-2 bg-[#FFE000] rounded-4 flex-col justify-center items-center inline-flex"
       type="button"
     >
       <div className="flex flex-col justify-start items-center gap-6 mobile:flex-row mobile:gap-4">
