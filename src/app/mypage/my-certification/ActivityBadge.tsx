d64d2d99bdba1732f9cb72c9e119e5ea7d4b89b5
spoonbill40082 <hj400820908@gmail.com>
2025-03-05 17:42:03 +0900

    refactor: popover.tsx 수정

--- src/app/mypage/my-certification/ActivityBadge.tsx
+++ src/app/mypage/my-certification/ActivityBadge.tsx
@@ -20,42 +20,46 @@ const ActivityBadge = () => {
         <div className="h-fit">
           <ul className="flex">
             <li>나의 대표 배지</li>
-            <li className="relative w-fit">
-              <IconHelp
-                onMouseEnter={() => setIsPopoverVisible1(true)}
-                onMouseLeave={() => setIsPopoverVisible1(false)}
-              />
-              {isPopoverVisible1 && (
-                <Popover
-                  label="대표 배지를 설정하고 프로필에 등록해 봐요"
-                  mode="light"
-                  tipDirection="down"
-                  tipPosition="center"
+            <ul>
+              <li className="relative w-fit">
+                <IconHelp
+                  onMouseEnter={() => setIsPopoverVisible1(true)}
+                  onMouseLeave={() => setIsPopoverVisible1(false)}
                 />
-              )}
-            </li>
+                {isPopoverVisible1 && (
+                  <Popover
+                    label="대표 배지를 설정하고 프로필에 등록해 봐요"
+                    mode="light"
+                    tipDirection="up"
+                    tipPosition="start"
+                  />
+                )}
+              </li>
+            </ul>
           </ul>
           <Badge variant="secondary" achieveItem="growthApprentice" achieve />
         </div>
         {/* 오버플로우는 추후 정돈할 것 */}
         <div className="h-fit">
-          <ul className="flex justify-between">
+          <ul className="flex justify-between bg-red-10">
             <ul className="flex">
               <li>전체 배지</li>
-              <li className="relative w-fit">
-                <IconHelp
-                  onMouseEnter={() => setIsPopoverVisible2(true)}
-                  onMouseLeave={() => setIsPopoverVisible2(false)}
-                />
-                {isPopoverVisible2 && (
-                  <Popover
-                    label="대표 배지를 설정하고 프로필에 등록해 봐요"
-                    mode="light"
-                    tipDirection="up"
-                    tipPosition="center"
+              <ul>
+                <li className="relative w-fit">
+                  <IconHelp
+                    onMouseEnter={() => setIsPopoverVisible2(true)}
+                    onMouseLeave={() => setIsPopoverVisible2(false)}
                   />
-                )}
-              </li>
+                  {isPopoverVisible2 && (
+                    <Popover
+                      label="대표 배지를 설정하고 프로필에 등록해 봐요"
+                      mode="light"
+                      tipDirection="down"
+                      tipPosition="start"
+                    />
+                  )}
+                </li>
+              </ul>
             </ul>
             <ul>
               <li className="flex flex-col items-center justify-center">
