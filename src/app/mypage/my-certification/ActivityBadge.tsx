1af54012476ed68491fb8a68aab118293c8f07a2
spoonbill40082 <hj400820908@gmail.com>
2025-03-06 13:33:17 +0900

    feat: 내 자격증 관리 페이지 제작 중

--- src/app/mypage/my-certification/ActivityBadge.tsx
+++ src/app/mypage/my-certification/ActivityBadge.tsx
@@ -9,71 +9,89 @@ import Badge from '@/components/Badge/Badge';
 interface AchievementForActivityBadge {
   achieveItem: string;
   achieve: boolean;
+  title: string;
 }
+
 const achievementsForActivityBadge: AchievementForActivityBadge[] = [
   {
     achieveItem: 'apprentice',
     achieve: true,
+    title: '견습생',
   },
   {
     achieveItem: 'expert',
     achieve: true,
+    title: '숙련자',
   },
   {
     achieveItem: 'master',
     achieve: false,
+    title: '달인',
   },
   {
     achieveItem: 'grandMaster',
     achieve: false,
+    title: '마스터',
   },
   {
     achieveItem: 'businessBrain',
     achieve: false,
+    title: '경영계 브레인',
   },
   {
     achieveItem: 'financeFairy',
     achieve: true,
+    title: '금융요정',
   },
   {
     achieveItem: 'knowledgeExplorer',
     achieve: false,
+    title: '지식탐험가',
   },
   {
     achieveItem: 'healthMentor',
     achieve: true,
+    title: '건강멘토',
   },
   {
     achieveItem: 'designMaster',
     achieve: false,
+    title: '디자인 마스터',
   },
   {
     achieveItem: 'machineEnthusiast',
     achieve: true,
+    title: '기계덕후',
   },
   {
     achieveItem: 'electronicsMaestro',
     achieve: false,
+    title: '전자 마에스트로',
   },
   {
     achieveItem: 'codingPro',
     achieve: false,
+    title: '코딩프로',
   },
   {
     achieveItem: 'globalCommunicator',
     achieve: false,
+    title: '글로벌 커뮤니케이터',
   },
   {
     achieveItem: 'sprinter',
     achieve: false,
+    title: '스프린터',
   },
   {
     achieveItem: 'communityChampion',
     achieve: true,
+    title: '커뮤니티 활동왕',
   },
   {
     achieveItem: 'growthApprentice',
     achieve: false,
+    title: '폭풍성장 견습생',
   },
 ];
 
@@ -160,7 +178,7 @@ const ActivityBadge = () => {
                           key={index}
                           achieveItem={item.achieveItem}
                           achieve={true}
-                          onClick={() => alert(`${item.achieveItem} 선택 ㄱ?`)}
+                          onClick={() => alert(`${item.title} 선택 ㄱ?`)}
                         />
                       ))}
                     </div>
--- src/app/mypage/my-certification/MyCertificationList.tsx
+++ src/app/mypage/my-certification/MyCertificationList.tsx
@@ -1,50 +1,145 @@
+'use client';
+
 import React from 'react';
 import { IconHexagons, IconPencil, IconPlus, IconTrash } from '@tabler/icons-react';
 import { Button } from '@/components/ui/button';
 
+const certificationList = [
+  {
+    name: '자격증명 1',
+    organization: '기관명 1',
+    date: '취득일 1',
+    expire: '만료일',
+    score: '-',
+    certificationNumber: '2025113255654',
+  },
+  {
+    name: '자격증명 2',
+    organization: '기관명 2',
+    date: '취득일 2',
+    expire: '만료일',
+    score: '500',
+    certificationNumber: '2025113255654',
+  },
+  {
+    name: '자격증명 3',
+    organization: '기관명 3',
+    date: '취득일 3',
+    expire: '만료일',
+    score: '500',
+    certificationNumber: '2025113255654',
+  },
+  {
+    name: '자격증명 4',
+    organization: '기관명 4',
+    date: '취득일 4',
+    expire: '만료일',
+    score: '-',
+    certificationNumber: '2025113255654',
+  },
+  {
+    name: '자격증명 5',
+    organization: '기관명 5',
+    date: '취득일 5',
+    expire: '만료일',
+    score: '500',
+    certificationNumber: '2025113255654',
+  },
+  {
+    name: '자격증명 6',
+    organization: '기관명 6',
+    date: '취득일 6',
+    expire: '만료일',
+    score: '-',
+    certificationNumber: '2025113255654',
+  },
+  {
+    name: '자격증명 7',
+    organization: '기관명 7',
+    date: '취득일 7',
+    expire: '만료일',
+    score: '-',
+    certificationNumber: '2025113255654',
+  },
+];
+
+const handleCertificationRegistration = () => {
+  confirm('테스트');
+};
+const handleCertificationReview = (name: string) => {
+  confirm(`${name} 후기 입력하기?`);
+};
+const handleCertificationEdit = (name: string) => {
+  confirm(`${name} 수정?`);
+};
+const handleCertificationDelete = (name: string) => {
+  confirm(`${name} 삭제제?`);
+};
+
 const MyCertificationList = () => {
   return (
-    <div>
-      <div className="flex w-full justify-between">
-        <ul className="flex">
-          <IconHexagons className="rotate-180" />
-          <li>내 자격증</li>
-        </ul>
-        <ul className="flex">
-          <li>자격증 등록하기</li>
-          <IconPlus />
-        </ul>
-      </div>
-      <div>
-        <ul className="flex">
-          <li>자격증명</li>
-          <li>기관명</li>
-          <li>취득일</li>
-          <li>만료일</li>
-          <li>점수</li>
-          <li>자격증 번호</li>
-        </ul>
-        <ul className="flex">
-          <li>자격증명 내용</li>
-          <li>기관명 내용</li>
-          <li>취득일 내용</li>
-          <li>만료일 내용</li>
-          <li>점수 내용</li>
-          <li>자격증 번호 내용</li>
-          <li>
-            <Button>후기 입력하기</Button>
-          </li>
-          <li>
-            <Button>
-              <IconPencil />
-            </Button>
-          </li>
-          <li>
-            <Button>
-              <IconTrash />
-            </Button>
-          </li>
-        </ul>
+    <div className="flex justify-center py-[64px]">
+      <div className="flex w-[1121px] flex-col gap-6">
+        <div className="flex w-full items-center justify-between">
+          <ul className="flex">
+            <IconHexagons className="rotate-180" />
+            <li className="text-title-medium-desktop">내 자격증</li>
+          </ul>
+          <ul
+            className="flex cursor-pointer items-center gap-2"
+            onClick={handleCertificationRegistration}
+          >
+            <li className="p-3 text-label-small-desktop text-blue-40">자격증 등록하기</li>
+            <IconPlus className="h-[20px] w-[20px] text-blue-40" />
+          </ul>
+        </div>
+        <div className="flex flex-col gap-6">
+          <ul className="flex text-body-medium-desktop text-neutral-30">
+            <li className="w-[220px] p-4">자격증명</li>
+            <li className="w-[160px] p-4">기관명</li>
+            <li className="w-[130px] p-4">취득일</li>
+            <li className="w-[130px] p-4">만료일</li>
+            <li className="w-[80px] p-4">점수</li>
+            <li className="w-[160px] p-4">자격증 번호</li>
+            <li className="w-[145px] p-4"></li>
+            <li className="w-[96px] p-4"></li>
+          </ul>
+          {certificationList.map((item, index) => (
+            <ul className="flex items-center" key={index}>
+              <li className="w-[220px] p-4 text-body-medium-desktop">{item.name}</li>
+              <li className="w-[160px] p-4 text-body-medium-desktop">{item.organization}</li>
+              <li className="w-[130px] p-4 text-label-medium-desktop">{item.date}</li>
+              <li className="w-[130px] p-4 text-label-medium-desktop">{item.expire}</li>
+              <li className="w-[80px] py-4 pl-0 pr-4 text-label-medium-desktop">
+                <p className="flex h-[30px] w-[55px] items-center justify-center rounded-4 bg-neutral-10">
+                  {item.score}
+                </p>
+              </li>
+              <li className="w-[160px] p-4 text-label-medium-desktop">
+                {item.certificationNumber}
+              </li>
+              <li className="w-[145px] p-4">
+                <Button
+                  className="h-[38px] w-[121px] rounded-4 bg-neutral-70 text-label-small-desktop text-neutral-0"
+                  onClick={() => handleCertificationReview(item.name)}
+                >
+                  후기 입력하기
+                </Button>
+              </li>
+              <li className="flex w-[96px] gap-4 p-4">
+                <div className="border border-r border-neutral-10"></div>
+                <IconPencil
+                  className="cursor-pointer text-neutral-20"
+                  onClick={() => handleCertificationEdit(item.name)}
+                />
+                <IconTrash
+                  className="cursor-pointer text-neutral-20"
+                  onClick={() => handleCertificationDelete(item.name)}
+                />
+              </li>
+            </ul>
+          ))}
+        </div>
       </div>
     </div>
   );
