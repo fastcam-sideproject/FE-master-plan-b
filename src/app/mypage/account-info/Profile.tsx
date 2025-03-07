cfb4c1024414f629a075af46682757ad754f8a30
spoonbill40082 <hj400820908@gmail.com>
2025-03-07 10:51:05 +0900

    feat: 내 정보 수정하기 페이지 프로필 제작 중

--- src/app/mypage/account-info/Profile.tsx
+++ src/app/mypage/account-info/Profile.tsx
@@ -3,11 +3,15 @@ import { Button } from '@/components/ui/button';
 
 const Profile = () => {
   return (
-    <div className="flex h-[252px] flex-col justify-between p-6">
+    <div className="flex h-[252px] flex-col justify-between bg-green-10 p-6">
       <div className="h-[100px] w-[100px] rounded-10 bg-red-10"></div>
       <div className="flex flex-col gap-4">
-        <Button>프로필 변경</Button>
-        <Button>로그아웃</Button>
+        <Button variant="secondary" size="sm">
+          프로필 변경
+        </Button>
+        <Button variant="secondary" size="sm">
+          로그아웃
+        </Button>
       </div>
     </div>
   );
