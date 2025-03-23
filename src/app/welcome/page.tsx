17b4b1b2be573833e9703572573b6b827f560325
spoonbill40082 <hj400820908@gmail.com>
2025-03-23 17:11:40 +0900

    feat: 회원가입 기능 제작

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,17 +1,33 @@
 'use client';
 
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Button } from '@/components/ui/button';
 
 export default function Page() {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const nickname =
+    searchParams.get('nickname') || '이 페이지에 어떻게 들어왔어?';
+
   return (
     <div className="my-40">
       <section className="flex flex-col items-center">
-        <h1 className="text-center text-heading-small-desktop mb-5">000님<br /> 환영합니다!</h1>
-        <p className="text-center text-neutral-50 mb-16">마스터 플랜비가 OOO님의 <br />
-          자격증 정보를 한 곳에서 관리해드릴게요</p>
-        <Button type="button" className="w-[360px]" variant={"secondary"} size={"md"} textColor={"gray"} label="온보딩 하러가기" onClick={() => router.push("/onbording")} />
+        <h1 className="mb-5 text-center text-heading-small-desktop">
+          {nickname}님<br /> 환영합니다!
+        </h1>
+        <p className="mb-16 text-center text-neutral-50">
+          마스터 플랜비가 {nickname}님의 <br />
+          자격증 정보를 한 곳에서 관리해드릴게요
+        </p>
+        <Button
+          type="button"
+          className="w-[360px]"
+          variant={'secondary'}
+          size={'md'}
+          textColor={'gray'}
+          label="온보딩 하러가기"
+          onClick={() => router.push('/onbording')}
+        />
       </section>
     </div>
   );
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -7,34 +7,103 @@ import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import AgreementCheckboxes from './AgreementCheckboxes';
 
-// signUpMasterplanApi.postVerification;
-// signUpMasterplanApi.sendVerificationCode;
-// signUpMasterplanApi.create;
-
 export default function SignUp() {
   const router = useRouter();
   const [isAgreementChecked, setIsAgreementChecked] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [passwordValidationError, setPasswordValidationError] = useState('');
   const [nickname, setNickname] = useState('');
+  const [emailExist, setEmailExist] = useState(false);
+  const [emailAvailable, setEmailAvailable] = useState(false);
 
   const handleEmailVerificationCode = async () => {
     if (!email) {
       alert('이메일을 입력해주세요');
       return;
     }
+
+    // 상태 초기화
+    setEmailExist(false);
+    setEmailAvailable(false);
+
     try {
       console.log('인증코드 발송 시도');
-      await signUpMasterplanApi.sendVerificationCode({ email });
+      console.log('요청 데이터:', { email });
+
+      // API 호출
+      const [error, response] = await signUpMasterplanApi.sendVerificationCode({
+        email,
+      });
+
+      if (error) {
+        // 에러 처리
+        if (error.response?.status === 409) {
+          setEmailExist(true);
+        } else {
+          alert(
+            '인증 코드 발송에 실패했습니다. 개발자 도구의 콘솔을 확인해주세요.',
+          );
+        }
+        return;
+      }
+
+      // 성공 시 실행
+      setEmailAvailable(true);
       alert('인증 코드가 발송되었습니다!');
-    } catch (error) {
-      alert('인증 코드 발송에 실패했습니다.');
-      console.error(error);
+    } catch (error: any) {
+      console.error('상세 에러:', error);
     }
+  };
+
+  const validatePasswordFormat = (password: string) => {
+    if (password.length < 8 || password.length > 16) {
+      setPasswordValidationError('비밀번호가 올바르지 않아요');
+      return false;
+    }
+
+    let containsUpperCase = /[A-Z]/.test(password);
+    let containsLowerCase = /[a-z]/.test(password);
+    let containsNumber = /[0-9]/.test(password);
+    let containsSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    let satisfiedConditions = [
+      containsUpperCase,
+      containsLowerCase,
+      containsNumber,
+      containsSpecial,
+    ].filter(Boolean).length;
+
+    if (satisfiedConditions < 3) {
+      setPasswordValidationError('비밀번호가 올바르지 않아요');
+      return false;
+    }
+
+    setPasswordValidationError('');
+    return true;
+  };
+
+  const validatePassword = (password: string, passwordConfirm: string) => {
+    if (!validatePasswordFormat(password)) {
+      return false;
+    }
+
+    if (passwordConfirm && password !== passwordConfirm) {
+      setPasswordError('비밀번호가 일치하지 않습니다');
+      return false;
+    }
+
+    setPasswordError('비밀번호가 일치합니다!');
+    return true;
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    if (!validatePassword(password, passwordConfirm)) {
+      return;
+    }
     try {
       await signUpMasterplanApi.create({
         email,
@@ -42,10 +111,17 @@ export default function SignUp() {
         nickname,
         isAgreed: isAgreementChecked,
       });
-      router.push('/welcome');
+      router.push(`/welcome?nickname=${encodeURIComponent(nickname)}`);
     } catch (error) {
       alert('회원가입 실패!');
       console.error(error);
+    }
+  };
+
+  const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value.length <= 16) {
+      setNickname(value);
     }
   };
 
@@ -60,8 +136,12 @@ export default function SignUp() {
             placeholder="닉네임을 입력해주세요"
             required
             value={nickname}
-            onChange={(e) => setNickname(e.target.value)}
+            onChange={handleNicknameChange}
+            maxLength={16}
           />
+          <p className="text-end text-body-xsmall-desktop font-[400] text-neutral-30">
+            {nickname.length}/16
+          </p>
         </label>
         <label className="flex flex-col gap-4">
           아이디
@@ -71,7 +151,11 @@ export default function SignUp() {
               placeholder="아이디를 입력해주세요"
               required
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setEmailExist(false);
+                setEmailAvailable(false);
+              }}
             />
             <Button
               label="인증하기"
@@ -81,6 +165,16 @@ export default function SignUp() {
               onClick={handleEmailVerificationCode}
             />
           </div>
+          {emailExist && (
+            <p className="text-body-xsmall-desktop text-red-40">
+              이미 가입된 이메일이에요
+            </p>
+          )}
+          {emailAvailable && (
+            <p className="text-body-xsmall-desktop text-blue-40">
+              사용 가능한 이메일이에요
+            </p>
+          )}
         </label>
         <label className="mb-10 flex flex-col gap-4">
           비밀번호
@@ -89,13 +183,37 @@ export default function SignUp() {
             placeholder="비밀번호를 입력해주세요"
             required
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              validatePasswordFormat(e.target.value);
+            }}
           />
           <Input
             type="password"
             placeholder="비밀번호를 다시 한번 입력해주세요"
             required
+            value={passwordConfirm}
+            onChange={(e) => {
+              setPasswordConfirm(e.target.value);
+              validatePassword(password, e.target.value);
+            }}
           />
+          {passwordValidationError && (
+            <p className="text-body-xsmall-desktop text-red-40">
+              {passwordValidationError}
+            </p>
+          )}
+          {passwordError && (
+            <p
+              className={`text-body-xsmall-desktop ${
+                passwordError === '비밀번호가 일치합니다!'
+                  ? 'text-blue-40'
+                  : 'text-red-40'
+              }`}
+            >
+              {passwordError}
+            </p>
+          )}
           <p className="text-body-xsmall-desktop text-neutral-30">
             영문 대소문자, 숫자, 특수문자를 3가지 이상으로 조합해 8자 이상
             <br /> 16자 이하로 입력해주세요
