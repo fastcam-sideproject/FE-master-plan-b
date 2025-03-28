94b89afc79a7f65eaffdf8ba6df4164c08fc397d
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2025-03-28 21:14:53 +0900

    refactor : 예외처리

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -150,7 +150,13 @@ export default function ItemCard({ type, data }: ItemCardProps) {
 
   return (
     <div className="flex flex-wrap gap-6">
-      {data.map((item) => renderCard(item))}
+      {data.length > 0 ? (
+        data.map((item) => renderCard(item))
+      ) : (
+        <div className="text-neutral-40 text-body-medium-desktop py-10 w-full text-center">
+          검색 결과가 없습니다.
+        </div>
+      )}
     </div>
   );
 }
