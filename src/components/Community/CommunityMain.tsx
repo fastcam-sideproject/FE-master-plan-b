79a4c5e9cd141dd3881c441ea10aab80c95b4d6c
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2025-03-28 20:07:12 +0900

    feat : 페이지 구성

--- src/components/Community/CommunityMain.tsx
+++ src/components/Community/CommunityMain.tsx
@@ -7,22 +7,15 @@ import MainButton from "../MainButton/MainButton";
 import MainCategory from "../main/MainCatstgory";
 import MainItemCard from "../main/MainItemCard";
 import SearchBar from "../SearchBar/SearchBar";
+import { LiveChip } from "../LiveChip";
+import Popover from "../Popover/Popover";
 
 function CommunityMain() {
   return <>
     <section className="w-full flex flex-col justify-center items-center bg-neutral-100 py-10">
       <div className="w-[85%] flex justify-evenly">
         <div className="flex flex-col gap-10">
-          <div className="flex justify-center items-center py-3 px-8 bg-neutral-80 rounded-full border-2 border-neutral-70">
-            <div>
-              <div className="flex gap-4">
-                <span>
-                  <p className="text-neutral-0 text-body-medium-desktop"> <IconPlus className="inline-block" /> 00,000명 참여중</p>
-                </span>
-                <IconCircleFilled className="text-red-50" />
-              </div>
-            </div>
-          </div>
+          <LiveChip />
           <div className="flex flex-col gap-7">
             <h1 className="text-neutral-0 text-heading-small-desktop">합격자 커뮤니티</h1>
             <div className="flex flex-col gap-3 text-neutral-20">
@@ -32,13 +25,14 @@ function CommunityMain() {
             </div>
           </div>
         </div>
-        <div>
-          {/* <Popover
-          label="대표 배지를 설정하고 프로필에 등록해 봐요"
-          mode="light"
-          tipDirection="up"\
-          tipPosition="center"
-        /> */}
+        <div className="relative">
+          <Popover
+            label="합격자 커뮤니티에 들어가면 이런점이 좋아요"
+            mode="big"
+            tipDirection="up"
+            tipPosition="start"
+            className="top-0 -left-52"
+          />
           <Image width={350} height={300} src={"/character/web/MainPage.png"} alt={""} />
         </div>
       </div>
